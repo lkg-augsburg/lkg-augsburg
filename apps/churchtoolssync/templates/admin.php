34587125094cf674e9de6e77209204151abd2f2e
Søren Liebich <soeren.Liebich@gmail.com>
2024-04-25 08:49:28 +0200

    chor: adding admin.php

---
+++ apps/churchtoolssync/templates/admin.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Sören Liebich <soeren.liebich@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+?>
+<div id="churchtoolssync" class="section">
+  <h1>ChurchTolls Integration</h1>
+  <p>Minions ipsum po kass underweaaar hahaha daa poulet tikka masala poulet tikka masala uuuhhh butt tank yuuu! Uuuhhh daa me want bananaaa! Potatoooo. Daa poopayee bappleees hahaha gelatooo tulaliloo la bodaaa aaaaaah para tú chasy potatoooo. Hahaha chasy bananaaaa bee do bee do bee do para tú pepete ti aamoo! Potatoooo. Aaaaaah baboiii ti aamoo! Tank yuuu! Ti aamoo! Baboiii daa hahaha. Baboiii uuuhhh hana dul sae me want bananaaa! Jiji. Tank yuuu! bananaaaa underweaaar me want bananaaa! Gelatooo. Pepete pepete tank yuuu! Aaaaaah butt wiiiii.</p>
+  <p>Para tú tank yuuu! Jeje chasy gelatooo butt jiji jeje bappleees. Gelatooo ti aamoo! Jeje po kass potatoooo hana dul sae bananaaaa tank yuuu! Tatata bala tu me want bananaaa! Tank yuuu! Aaaaaah po kass hahaha. Hana dul sae tulaliloo hana dul sae gelatooo aaaaaah po kass para tú la bodaaa bananaaaa uuuhhh poopayee. Tank yuuu! underweaaar pepete potatoooo hahaha pepete poopayee aaaaaah. Potatoooo belloo! Po kass ti aamoo! Para tú. Jeje bananaaaa para tú pepete tank yuuu! Poopayee aaaaaah jeje poulet tikka masala butt belloo! Gelatooo tatata bala tu poopayee pepete baboiii aaaaaah jiji daa wiiiii. Gelatooo baboiii la bodaaa hana dul sae.</p>
+  <p>Poulet tikka masala me want bananaaa! Tulaliloo pepete. Poopayee bee do bee do bee do chasy jeje. Bappleees para tú jeje la bodaaa ti aamoo! Aaaaaah. Daa bananaaaa bananaaaa hana dul sae. Underweaaar tatata bala tu tatata bala tu bee do bee do bee do pepete gelatooo. Pepete ti aamoo! Underweaaar ti aamoo! Gelatooo. Ti aamoo! hahaha chasy bananaaaa.</p>
+  <p>Belloo! tatata bala tu la bodaaa underweaaar po kass ti aamoo! La bodaaa aaaaaah pepete. Baboiii daa potatoooo bananaaaa. Jiji bee do bee do bee do poopayee potatoooo aaaaaah ti aamoo! Butt la bodaaa gelatooo belloo! Po kass underweaaar wiiiii potatoooo aaaaaah baboiii tatata bala tu. Tatata bala tu jiji tank yuuu! Baboiii baboiii butt hana dul sae daa wiiiii jeje.</p>
+</div>
